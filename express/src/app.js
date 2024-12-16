20fdffeb6c7f3771c8c8da2f97eaaf4edb2b0c49
wolloh <67105296+wolloh@users.noreply.github.com>
2024-12-16 21:50:42 +0300

    Update app.js

--- express/src/app.js
+++ express/src/app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import TelegramBot from 'node-telegram-bot-api'
 import cors from 'cors'
 
-const token = '7214812115:AAG3SKiB1G4CZP1AkXrJkZyOWqlMd9_JXUY'
+const token = ''
 const bot = new TelegramBot(token, {
 
   polling: true
